0ab7b4053bde0e77fb4c1c808a5e8534ff78e827
luiscoimbra <programador.luis@gmail.com>
2013-09-27 12:05:21 -0300

    test without node_modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
 module.exports = function(grunt) {
-	
-	grunt.initConfig({
-		jshint: {
-			all: ['Gruntfile.js', 'hello.js']
-		}
-	});
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', 'jshint');
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js', 'hello.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('default', 'jshint');
 
 };
